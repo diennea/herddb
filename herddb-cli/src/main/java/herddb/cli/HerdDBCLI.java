c4b51ad872e3e075e7139f9ec55fbcdad6f1d1c1
eolivelli <eolivelli@gmail.com>
2016-07-15 17:20:46 +0200

    dump tablespace command

--- herddb-cli/src/main/java/herddb/cli/HerdDBCLI.java
+++ herddb-cli/src/main/java/herddb/cli/HerdDBCLI.java
@@ -18,13 +18,25 @@ package herddb.cli;
 import groovy.lang.Binding;
 import groovy.lang.GroovyShell;
 import herddb.client.ClientConfiguration;
+import herddb.client.HDBConnection;
+import herddb.client.TableSpaceDumpReceiver;
+import herddb.jdbc.HerdDBConnection;
 import herddb.jdbc.HerdDBDataSource;
+import herddb.log.LogSequenceNumber;
+import herddb.model.Record;
+import herddb.model.Table;
 import herddb.model.TableSpace;
+import herddb.storage.DataStorageManagerException;
+import herddb.utils.ExtendedDataOutputStream;
+import herddb.utils.SimpleBufferedOutputStream;
 import java.io.File;
 import java.io.IOException;
+import java.io.OutputStream;
 import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
+import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.nio.file.StandardOpenOption;
 import java.sql.Connection;
 import java.sql.ResultSet;
 import java.sql.ResultSetMetaData;
@@ -34,8 +46,10 @@ import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.concurrent.CountDownLatch;
 import java.util.logging.LogManager;
 import java.util.stream.Collectors;
+import javax.xml.ws.Holder;
 import org.apache.commons.cli.DefaultParser;
 import org.apache.commons.cli.HelpFormatter;
 import org.apache.commons.cli.Options;
@@ -57,20 +71,23 @@ public class HerdDBCLI {
         options.addOption("v", "verbose", false, "Verbose output");
         options.addOption("s", "schema", true, "Default tablespace (SQL schema)");
         options.addOption("f", "file", true, "SQL Script to execute (statement separated by 'GO' lines)");
-        options.addOption("g", "script", true, "Grovy Script to execute");
+        options.addOption("g", "script", true, "Groovy Script to execute");
         options.addOption("i", "ignoreerrors", false, "Ignore SQL Errors during file execution");
+        options.addOption("d", "dump", true, "Dump tablespace");
         org.apache.commons.cli.CommandLine commandLine;
         try {
             commandLine = parser.parse(options, args);
         } catch (ParseException error) {
-            System.out.println("Syntax error: " + error);
+            println("Syntax error: " + error);
             HelpFormatter formatter = new HelpFormatter();
             formatter.printHelp("herddb", options, true);
+            System.exit(1);
             return;
         }
         if (args.length == 0) {
             HelpFormatter formatter = new HelpFormatter();
             formatter.printHelp("herddb", options, true);
+            System.exit(1);
             return;
         }
 
@@ -84,6 +101,8 @@ public class HerdDBCLI {
         String schema = commandLine.getOptionValue("schema", TableSpace.DEFAULT);
         String query = commandLine.getOptionValue("query", "");
         String file = commandLine.getOptionValue("file", "");
+        String dump = commandLine.getOptionValue("dump", "");
+        println("dump:" + dump);
         String script = commandLine.getOptionValue("script", "");
         boolean ignoreerrors = commandLine.hasOption("ignoreerrors");
         try (HerdDBDataSource datasource = new HerdDBDataSource()) {
@@ -96,8 +115,7 @@ public class HerdDBCLI {
                 connection.setSchema(schema);
                 if (!query.isEmpty()) {
                     executeStatement(verbose, ignoreerrors, query, statement);
-                }
-                if (!file.isEmpty()) {
+                } else if (!file.isEmpty()) {
                     StringBuilder currentStatement = new StringBuilder();
                     for (String line : Files.readAllLines(Paths.get(file), StandardCharsets.UTF_8)) {
                         if (line.trim().equalsIgnoreCase("GO")) {
@@ -108,23 +126,44 @@ public class HerdDBCLI {
                         }
                     }
                     executeStatement(verbose, ignoreerrors, currentStatement.toString(), statement);
-                }
-                if (!script.isEmpty()) {
+                } else if (!script.isEmpty()) {
                     Map<String, Object> variables = new HashMap<>();
                     variables.put("connection", connection);
                     variables.put("datasource", datasource);
                     variables.put("statement", statement);
                     GroovyShell shell = new GroovyShell(new Binding(variables));
                     shell.evaluate(new File(script));
+                } else if (!dump.isEmpty()) {
+                    List<String> tablesToDump = new ArrayList<>();
+                    try (ResultSet rs = statement.executeQuery("SELECT table_name FROM " + schema + ".systables")) {
+                        while (rs.next()) {
+                            String tablename = rs.getString(1).toLowerCase();
+                            tablesToDump.add(tablename);
+                        }
+                    }
+                    Path outputfile = Paths.get(dump).toAbsolutePath();
+                    println("Dumping tables " + tablesToDump + " from tablespace " + schema + " to " + outputfile);
+
+                    try (OutputStream fout = Files.newOutputStream(outputfile, StandardOpenOption.CREATE_NEW);
+                            SimpleBufferedOutputStream oo = new SimpleBufferedOutputStream(fout);
+                            ExtendedDataOutputStream out = new ExtendedDataOutputStream(fout)) {
+                        dumpTablespace(schema, connection, out);
+                    }
+                } else {
+                    HelpFormatter formatter = new HelpFormatter();
+                    formatter.printHelp("herddb", options, true);
+                    System.exit(1);
+                    return;
                 }
             }
 
-        } catch (SQLException | IOException error) {
+        } catch (Exception error) {
             if (verbose) {
                 error.printStackTrace();
             } else {
-                System.out.println("error:" + error);
+                println("error:" + error);
             }
+            System.exit(1);
         }
     }
 
@@ -165,10 +204,81 @@ public class HerdDBCLI {
             }
         } catch (SQLException err) {
             if (ignoreerrors) {
-                System.out.println("ERROR:" + err);
+                println("ERROR:" + err);
             } else {
                 throw err;
             }
         }
+    }
+
+    private static void dumpTablespace(String schema, Connection connection, ExtendedDataOutputStream out) throws Exception {
+        HerdDBConnection hcon = connection.unwrap(HerdDBConnection.class);
+        HDBConnection hdbconnection = hcon.getConnection();
+        Holder<Throwable> errorHolder = new Holder<>();
+        CountDownLatch waiter = new CountDownLatch(1);
+        hdbconnection.dumpTableSpace(schema, new TableSpaceDumpReceiver() {
+
+            long tableCount;
+
+            @Override
+            public void onError(Throwable error) throws DataStorageManagerException {
+                errorHolder.value = error;
+                waiter.countDown();
+            }
+
+            @Override
+            public void finish() throws DataStorageManagerException {
+                waiter.countDown();
+            }
+
+            @Override
+            public void endTable() throws DataStorageManagerException {
+                println("endTable, records " + tableCount);
+                try {
+                    out.writeVInt(Integer.MIN_VALUE); // EndOfTableMarker
+                } catch (IOException err) {
+                    throw new DataStorageManagerException(err);
+                }
+            }
+
+            @Override
+            public void receiveTableDataChunk(List<Record> record) throws DataStorageManagerException {
+                try {
+                    out.writeVInt(record.size());
+                    for (Record r : record) {
+                        out.writeArray(r.key.data);
+                        out.writeArray(r.value.data);
+                    }
+                    tableCount += record.size();
+                } catch (IOException err) {
+                    throw new DataStorageManagerException(err);
+                }
+            }
+
+            @Override
+            public void beginTable(Table table) throws DataStorageManagerException {
+                println("beginTable " + table.name);
+                tableCount = 0;
+                try {
+                    out.writeArray(table.serialize());
+                } catch (IOException err) {
+                    throw new DataStorageManagerException(err);
+                }
+            }
+
+            @Override
+            public void start(LogSequenceNumber logSequenceNumber) throws DataStorageManagerException {
+                println("Backup at log position: " + logSequenceNumber);
+            }
+
+        }, 64 * 1024);
+        if (errorHolder.value != null) {
+            throw new Exception(errorHolder.value);
+        }
+        waiter.await();
+    }
+
+    private static void println(Object msg) {
+        System.out.println(msg);
     }
 }
