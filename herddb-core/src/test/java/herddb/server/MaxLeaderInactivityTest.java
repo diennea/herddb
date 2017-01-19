8b4c25216bc84388529c549e2020485af18e740f
eolivelli <eolivelli@gmail.com>
2017-01-19 09:30:49 +0100

    fix flacky test

--- herddb-core/src/test/java/herddb/server/MaxLeaderInactivityTest.java
+++ herddb-core/src/test/java/herddb/server/MaxLeaderInactivityTest.java
@@ -174,7 +174,7 @@ public class MaxLeaderInactivityTest {
             try (Server server_2 = new Server(serverconfig_2)) {
                 server_2.start();
 
-                TestUtils.execute(server_1.getManager(), "CREATE TABLESPACE 'ttt','leader:" + server_2.getNodeId() + "','expectedreplicacount:2','wait:60000','maxLeaderInactivityTime:5000'", Collections.emptyList());
+                TestUtils.execute(server_1.getManager(), "CREATE TABLESPACE 'ttt','leader:" + server_2.getNodeId() + "','expectedreplicacount:2','wait:60000'", Collections.emptyList());
 
                 // wait for server_2 to wake up
                 for (int i = 0; i < 100; i++) {
@@ -184,6 +184,8 @@ public class MaxLeaderInactivityTest {
                     Thread.sleep(500);
                 }
                 assertTrue(server_2.getManager().getTableSpaceManager("ttt") != null && server_2.getManager().getTableSpaceManager("ttt").isLeader());
+
+                TestUtils.execute(server_1.getManager(), "ALTER TABLESPACE 'ttt','maxLeaderInactivityTime:5000'", Collections.emptyList());
 
                 try (DataScanner scan = scan(server_1.getManager(), "SELECT * FROM SYSTABLESPACEREPLICASTATE where "
                     + "tablespace_name='ttt' and nodeId='" + server_2.getNodeId() + "'", Collections.emptyList());) {
