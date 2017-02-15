cd3330f21776f85894e6e0502d64edd602b5f61a
eolivelli <eolivelli@gmail.com>
2017-02-15 16:07:44 +0100

    YCSB runner script

--- ycsb-runner/run-ycsb.sh
+++ ycsb-runner/run-ycsb.sh
@@ -23,5 +23,6 @@ $HERDDB_PATH/bin/herddb-cli.sh -q "DROP TABLE usertable"
 $HERDDB_PATH/bin/herddb-cli.sh -q "CREATE TABLE usertable ( YCSB_KEY VARCHAR(191) NOT NULL, FIELD0 STRING, FIELD1 STRING, FIELD2 STRING, FIELD3 STRING, FIELD4 STRING, FIELD5 STRING, FIELD6 STRING, FIELD7 STRING, FIELD8 STRING, FIELD9 STRING, PRIMARY KEY (YCSB_KEY));"
 
 $YCSB_PATH/bin/ycsb load jdbc -P $YCSB_PATH/workloads/$WORKLOAD -P $HERE/herd.properties -cp $HERDDB_PATH/herddb-jdbc* -threads 200 -s
+$YCSB_PATH/bin/ycsb run jdbc -P $YCSB_PATH/workloads/$WORKLOAD -P $HERE/herd.properties -cp $HERDDB_PATH/herddb-jdbc* -threads 200 -s
 
 $HERDDB_PATH/bin/service server kill
