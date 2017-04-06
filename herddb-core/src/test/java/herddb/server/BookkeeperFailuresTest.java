05b580086842b6b80976b79f495fc0c7513869bd
eolivelli <eolivelli@gmail.com>
2017-04-06 12:36:48 +0200

    Closing ledger after failure on AddEntry may lead to data loss #100

--- herddb-core/src/test/java/herddb/server/BookkeeperFailuresTest.java
+++ herddb-core/src/test/java/herddb/server/BookkeeperFailuresTest.java
@@ -39,7 +39,6 @@ import herddb.model.commands.InsertStatement;
 import herddb.utils.ZKTestEnv;
 import java.nio.charset.StandardCharsets;
 import java.util.Collections;
-import java.util.List;
 import java.util.logging.ConsoleHandler;
 import java.util.logging.Level;
 import java.util.logging.SimpleFormatter;
@@ -53,7 +52,6 @@ import static org.junit.Assert.assertNotSame;
 import static org.junit.Assert.assertTrue;
 import static org.junit.Assert.fail;
 import org.junit.Before;
-import org.junit.Ignore;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.rules.TemporaryFolder;
@@ -64,26 +62,6 @@ import org.junit.rules.TemporaryFolder;
  * @author enrico.olivelli
  */
 public class BookkeeperFailuresTest {
-
-    @Before
-    public void setupLogger() throws Exception {
-        Level level = Level.FINEST;
-        Thread.setDefaultUncaughtExceptionHandler(new Thread.UncaughtExceptionHandler() {
-
-            @Override
-            public void uncaughtException(Thread t, Throwable e) {
-                System.err.println("uncaughtException from thread " + t.getName() + ": " + e);
-                e.printStackTrace();
-            }
-        });
-        java.util.logging.LogManager.getLogManager().reset();
-        ConsoleHandler ch = new ConsoleHandler();
-        ch.setLevel(level);
-        SimpleFormatter f = new SimpleFormatter();
-        ch.setFormatter(f);
-        java.util.logging.Logger.getLogger("").setLevel(level);
-        java.util.logging.Logger.getLogger("").addHandler(ch);
-    }
 
     @Rule
     public TemporaryFolder folder = new TemporaryFolder();
@@ -365,105 +343,4 @@ public class BookkeeperFailuresTest {
         }
     }
 
-    @Test
-    public void testBookieNotAvailableDuringTransaction() throws Exception {
-        ServerConfiguration serverconfig_1 = new ServerConfiguration(folder.newFolder().toPath());
-        serverconfig_1.set(ServerConfiguration.PROPERTY_NODEID, "server1");
-        serverconfig_1.set(ServerConfiguration.PROPERTY_PORT, 7867);
-        serverconfig_1.set(ServerConfiguration.PROPERTY_MODE, ServerConfiguration.PROPERTY_MODE_CLUSTER);
-        serverconfig_1.set(ServerConfiguration.PROPERTY_ZOOKEEPER_ADDRESS, testEnv.getAddress());
-        serverconfig_1.set(ServerConfiguration.PROPERTY_ZOOKEEPER_PATH, testEnv.getPath());
-        serverconfig_1.set(ServerConfiguration.PROPERTY_ZOOKEEPER_SESSIONTIMEOUT, testEnv.getTimeout());
-        serverconfig_1.set(ServerConfiguration.PROPERTY_ENFORCE_LEADERSHIP, false);
-
-        try (Server server = new Server(serverconfig_1)) {
-            server.start();
-            server.waitForStandaloneBoot();
-            Table table = Table.builder()
-                .name("t1")
-                .column("c", ColumnTypes.INTEGER)
-                .primaryKey("c")
-                .build();
-            server.getManager().executeStatement(new CreateTableStatement(table), StatementEvaluationContext.DEFAULT_EVALUATION_CONTEXT(), TransactionContext.NO_TRANSACTION);
-
-            StatementExecutionResult executeStatement = server.getManager().executeUpdate(new InsertStatement(TableSpace.DEFAULT, "t1", RecordSerializer.makeRecord(table, "c", 1)), StatementEvaluationContext.DEFAULT_EVALUATION_CONTEXT(), TransactionContext.AUTOTRANSACTION_TRANSACTION);
-            long transactionId = executeStatement.transactionId;
-
-            server.getManager().executeUpdate(new InsertStatement(TableSpace.DEFAULT, "t1", RecordSerializer.makeRecord(table, "c", 2)), StatementEvaluationContext.DEFAULT_EVALUATION_CONTEXT(), new TransactionContext(transactionId));
-
-            server.getManager().executeUpdate(new InsertStatement(TableSpace.DEFAULT, "t1", RecordSerializer.makeRecord(table, "c", 3)), StatementEvaluationContext.DEFAULT_EVALUATION_CONTEXT(), new TransactionContext(transactionId));
-            TableSpaceManager tableSpaceManager = server.getManager().getTableSpaceManager(TableSpace.DEFAULT);
-            BookkeeperCommitLog log = (BookkeeperCommitLog) tableSpaceManager.getLog();
-            long ledgerId = log.getLastSequenceNumber().ledgerId;
-            assertTrue(ledgerId >= 1);
-
-            Transaction transaction = tableSpaceManager.getTransactions().stream().filter(t -> t.transactionId == transactionId).findFirst().get();
-            transaction.synch();
-
-            try (DataScanner scan = scan(server.getManager(), "select * from t1", Collections.emptyList(), new TransactionContext(transactionId));) {
-                assertEquals(3, scan.consume().size());
-            }
-
-            try (DataScanner scan = scan(server.getManager(), "select * from t1", Collections.emptyList(), TransactionContext.NO_TRANSACTION);) {
-                // no record, but the table exists!
-                assertEquals(0, scan.consume().size());
-            }
-
-            // we do not want auto-recovery
-            server.getManager().setActivatorPauseStatus(true);
-
-            testEnv.stopBookie();
-
-            // transaction will continue and see the failure only the time of the commit
-            try {
-                server.getManager().executeUpdate(new InsertStatement(TableSpace.DEFAULT, "t1", RecordSerializer.makeRecord(table, "c", 4)), StatementEvaluationContext.DEFAULT_EVALUATION_CONTEXT(), new TransactionContext(transactionId));
-                server.getManager().executeUpdate(new InsertStatement(TableSpace.DEFAULT, "t1", RecordSerializer.makeRecord(table, "c", 5)), StatementEvaluationContext.DEFAULT_EVALUATION_CONTEXT(), new TransactionContext(transactionId));
-                server.getManager().executeUpdate(new InsertStatement(TableSpace.DEFAULT, "t1", RecordSerializer.makeRecord(table, "c", 6)), StatementEvaluationContext.DEFAULT_EVALUATION_CONTEXT(), new TransactionContext(transactionId));
-            } catch (StatementExecutionException expected) {
-                // in can happen that the log gets closed
-                assertEquals(herddb.log.LogNotAvailableException.class, expected.getCause().getClass());
-            }
-
-            try {
-                server.getManager().executeStatement(new CommitTransactionStatement(TableSpace.DEFAULT, transactionId), StatementEvaluationContext.DEFAULT_EVALUATION_CONTEXT(), TransactionContext.NO_TRANSACTION);
-                // this will fail alweays
-                fail();
-            } catch (StatementExecutionException expected) {
-            }
-
-            testEnv.startBookie(false);
-
-            while (true) {
-                System.out.println("status leader:" + tableSpaceManager.isLeader() + " failed:" + tableSpaceManager.isFailed());
-                if (tableSpaceManager.isFailed()) {
-                    break;
-                }
-                Thread.sleep(100);
-            }
-            server.getManager().setActivatorPauseStatus(false);
-            server.getManager().triggerActivator(ActivatorRunRequest.TABLESPACEMANAGEMENT);
-
-            while (true) {
-                TableSpaceManager tableSpaceManager_after_failure = server.getManager().getTableSpaceManager(TableSpace.DEFAULT);
-                System.out.println("tableSpaceManager_after_failure:" + tableSpaceManager_after_failure);
-                System.out.println("tableSpaceManager:" + tableSpaceManager);
-                if (tableSpaceManager_after_failure != null && tableSpaceManager_after_failure != tableSpaceManager) {
-                    break;
-                }
-                Thread.sleep(1000);
-                server.getManager().triggerActivator(ActivatorRunRequest.TABLESPACEMANAGEMENT);
-            }
-
-            TableSpaceManager tableSpaceManager_after_failure = server.getManager().getTableSpaceManager(TableSpace.DEFAULT);
-            Assert.assertNotNull(tableSpaceManager_after_failure);
-            assertNotSame(tableSpaceManager_after_failure, tableSpaceManager);
-            assertTrue(!tableSpaceManager_after_failure.isFailed());
-
-            server.getManager().executeUpdate(new InsertStatement(TableSpace.DEFAULT, "t1", RecordSerializer.makeRecord(table, "c", 4)), StatementEvaluationContext.DEFAULT_EVALUATION_CONTEXT(), TransactionContext.NO_TRANSACTION);
-
-            try (DataScanner scan = scan(server.getManager(), "select * from t1", Collections.emptyList());) {
-                assertEquals(1, scan.consume().size());
-            }
-        }
-    }
 }
