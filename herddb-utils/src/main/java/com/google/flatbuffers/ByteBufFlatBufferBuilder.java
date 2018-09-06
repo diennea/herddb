bfc6022612344172180ad492283b1d4aded52c01
Enrico Olivelli - Diennea <enrico.olivelli@diennea.com>
2018-09-06 17:32:00 +0200

    fix license header

--- herddb-utils/src/main/java/com/google/flatbuffers/ByteBufFlatBufferBuilder.java
+++ herddb-utils/src/main/java/com/google/flatbuffers/ByteBufFlatBufferBuilder.java
@@ -1,14 +1,31 @@
+/*
+ Licensed to Diennea S.r.l. under one
+ or more contributor license agreements. See the NOTICE file
+ distributed with this work for additional information
+ regarding copyright ownership. Diennea S.r.l. licenses this file
+ to you under the Apache License, Version 2.0 (the
+ "License"); you may not use this file except in compliance
+ with the License.  You may obtain a copy of the License at
+
+ http://www.apache.org/licenses/LICENSE-2.0
+
+ Unless required by applicable law or agreed to in writing,
+ software distributed under the License is distributed on an
+ "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ KIND, either express or implied.  See the License for the
+ specific language governing permissions and limitations
+ under the License.
+
+ */
 package com.google.flatbuffers;
 
 import static com.google.flatbuffers.FlatBufferBuilder.growByteBuffer;
-import herddb.network.MessageWrapper;
 import io.netty.buffer.ByteBuf;
 import io.netty.buffer.PooledByteBufAllocator;
 import io.netty.util.Recycler;
 import java.nio.ByteBuffer;
 import java.nio.ByteOrder;
 import java.util.IdentityHashMap;
-import java.util.function.Consumer;
 
 /**
  * A special FlatBufferBuilder able to write to Netty ByteBufs
