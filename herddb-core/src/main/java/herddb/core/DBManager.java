610c4b5ca1cc70c6b957a8e98ec892ddf91a067c
Enrico Olivelli <eolivelli@apache.org>
2018-10-14 13:28:46 +0200

    16 to 64

--- herddb-core/src/main/java/herddb/core/DBManager.java
+++ herddb-core/src/main/java/herddb/core/DBManager.java
@@ -167,7 +167,7 @@ public class DBManager implements AutoCloseable, MetadataChangeListener {
             CommitLogManager commitLogManager, Path tmpDirectory, herddb.network.ServerHostData hostData, ServerConfiguration configuration) {
         this.serverConfiguration = configuration;
         this.tmpDirectory = tmpDirectory;
-        this.callbacksExecutor = Executors.newWorkStealingPool(16);
+        this.callbacksExecutor = Executors.newWorkStealingPool(64);
         this.recordSetFactory = dataStorageManager.createRecordSetFactory();
         this.metadataStorageManager = metadataStorageManager;
         this.dataStorageManager = dataStorageManager;
