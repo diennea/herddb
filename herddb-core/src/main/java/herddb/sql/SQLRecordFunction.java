b9f40dd88ba179652a76afb4d1ddbb514f7b26e2
Enrico Olivelli <eolivelli@gmail.com>
2018-10-25 14:53:14 +0200

    Do not allocate HashMaps during updates and inserts (#282)
    * Do not allocate HashMaps during updates and inserts
    
    * Drop old method

--- herddb-core/src/main/java/herddb/sql/SQLRecordFunction.java
+++ herddb-core/src/main/java/herddb/sql/SQLRecordFunction.java
@@ -20,7 +20,6 @@
 package herddb.sql;
 
 import herddb.codec.RecordSerializer;
-import static herddb.codec.RecordSerializer.serializeValue;
 import herddb.model.Record;
 import herddb.model.RecordFunction;
 import herddb.model.StatementEvaluationContext;
@@ -33,6 +32,7 @@ import herddb.utils.DataAccessor;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.function.Function;
 import java.util.stream.Collectors;
 import net.sf.jsqlparser.schema.Column;
 
@@ -44,39 +44,61 @@ import net.sf.jsqlparser.schema.Column;
 public class SQLRecordFunction extends RecordFunction {
 
     private final Table table;
-    private final List<String> columns;
-    private final List<CompiledSQLExpression> expressions;
+    private final Map<String, CompiledSQLExpression> expressionsByColumnName;
 
     public SQLRecordFunction(Table table, List<Column> columns, List<CompiledSQLExpression> expressions) {
         this.table = table;
-        this.columns = columns.stream().map(Column::getColumnName).collect(Collectors.toList());
-        this.expressions = expressions;
+        List<String> _columns = columns.stream().map(Column::getColumnName).collect(Collectors.toList());
+        this.expressionsByColumnName = new HashMap<>();
+        final int size = _columns.size();
+        for (int i = 0; i < size; i++) {
+            expressionsByColumnName.put(_columns.get(i), expressions.get(i));
+        }
     }
 
     public SQLRecordFunction(List<String> columns, Table table, List<CompiledSQLExpression> expressions) {
         this.table = table;
-        this.columns = columns;
-        this.expressions = expressions;
+        this.expressionsByColumnName = new HashMap<>();
+        final int size = columns.size();
+        for (int i = 0; i < size; i++) {
+            expressionsByColumnName.put(columns.get(i), expressions.get(i));
+        }
     }
 
     @Override
     public byte[] computeNewValue(Record previous, StatementEvaluationContext context, TableContext tableContext) throws StatementExecutionException {
         try {
-            Map<String, Object> res = previous != null ? new HashMap<>(previous.toBean(table)) : new HashMap<>(table.columns.length);
-            DataAccessor bean = previous != null ? previous.getDataAccessor(table) : DataAccessor.NULL;
-            final int size = columns.size();
-            for (int i = 0; i < size; i++) {
-                CompiledSQLExpression e = expressions.get(i);
-                String columnName = columns.get(i);
-                herddb.model.Column column = table.getColumn(columnName);
-                if (column == null) {
-                    throw new StatementExecutionException("unknown column " + columnName + " in table " + table.name);
-                }
-                columnName = column.name;
-                Object value = RecordSerializer.convert(column.type, e.evaluate(bean, context));
-                res.put(columnName, value);
+            if (previous != null) {
+                DataAccessor bean = previous.getDataAccessor(table);
+                Function<String, Object> fieldValueComputer = (columnName) -> {
+                    CompiledSQLExpression e = expressionsByColumnName.get(columnName);
+                    if (e == null) {
+                        return bean.get(columnName);
+                    } else {
+                        herddb.model.Column column = table.getColumn(columnName);
+                        if (column == null) {
+                            throw new StatementExecutionException("unknown column " + columnName + " in table " + table.name);
+                        }
+                        return RecordSerializer.convert(column.type, e.evaluate(bean, context));
+                    }
+                };
+                return RecordSerializer.buildRecord(previous.value != null ? previous.value.data.length : 0, table, fieldValueComputer);
+            } else {
+                Function<String, Object> fieldValueComputer = (columnName) -> {
+                    CompiledSQLExpression e = expressionsByColumnName.get(columnName);
+                    if (e == null) {
+                        return null;
+                    } else {
+                        herddb.model.Column column = table.getColumn(columnName);
+                        if (column == null) {
+                            throw new StatementExecutionException("unknown column " + columnName + " in table " + table.name);
+                        }
+                        return RecordSerializer.convert(column.type, e.evaluate(DataAccessor.NULL, context));
+                    }
+                };
+                return RecordSerializer.buildRecord(0, table, fieldValueComputer);
             }
-            return RecordSerializer.serializeValueRaw(res, table);
+
         } catch (IllegalArgumentException err) {
             throw new StatementExecutionException(err);
         }
@@ -84,7 +106,7 @@ public class SQLRecordFunction extends RecordFunction {
 
     @Override
     public String toString() {
-        return "SQLRecordFunction{" + "table=" + table + ", columns=" + columns + ", expressions=" + expressions + '}';
+        return "SQLRecordFunction{" + "table=" + table + ", exp=" + expressionsByColumnName + '}';
     }
 
 }
