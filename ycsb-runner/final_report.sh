db95e5a3b9bd629350b63c848af59049092e45b4
Hamado Dene <hamado.dene@diennea.com>
2018-10-25 17:29:57 +0200

    Fix varius

--- ycsb-runner/final_report.sh
+++ ycsb-runner/final_report.sh
@@ -2,30 +2,39 @@
 
 WORKLOAD=$1
 NAME="_final_report.txt"
-JAVA_VERSION="java 10"
 SYSTEM=$(uname -s -r)
+RAM=$(dmidecode -t 16 | grep "Maximum Capacity" | cut -d ':'  -f2)
 DATE=$(date)
+PROCESSOR=
 THROUGHPUT=$2
 ATTEMPTS=$3
 REPORT="FULL_TOTAL_REPORT.txt"
+
+
+cat /proc/cpuinfo | grep "model name" > processor.txt
+PROCESSOR=$(tail -1 processor.txt | cut -d ':' -f2)
+echo $PROCESSOR
 
 if [[ ! -e $WORKLOAD$NAME ]]; then
         touch $WORKLOAD$NAME
 else
 	rm -rf  $WORKLOAD$NAME
-	touch  $WORKLOAD$NAME
+	touch $WORKLOAD$NAME
 fi
 
 echo -e " " >> $WORKLOAD$NAME
 echo -e "------------------------------------------------------------------- " >> $WORKLOAD$NAME
-echo "WORKLOAD = $WORKLOAD" >>  $WORKLOAD$NAME
+echo "WORKLOAD = $WORKLOAD" >> $WORKLOAD$NAME
 echo "SYSTEM= $SYSTEM" >> $WORKLOAD$NAME
-echo "JAVA_VERSION= $JAVA_VERSION" >> $WORKLOAD$NAME
+echo "RAM= $RAM" >> $WORKLOAD$NAME
+echo "PROCESSOR = $PROCESSOR" >> $WORKLOAD$NAME
+echo "JAVA_VERSION= $JAVA_HOME" >> $WORKLOAD$NAME
 echo "DATE= $DATE" >> $WORKLOAD$NAME
-echo "THROUGHPUT = $THROUGHPUT" >> $WORKLOAD$NAME
+echo "THROUGHPUT = $THROUGHPUT" >>$WORKLOAD$NAME
 echo "ATTEMPTS = $ATTEMPTS"  >> $WORKLOAD$NAME
 
 
 cat $WORKLOAD$NAME >> $REPORT 
-
-
+rm -rf processor.txt
+rm -rf $WORKLOAD$NAME
+ 
