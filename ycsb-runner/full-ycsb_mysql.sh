4c343897eb4403df272d32364307b3d6adf3932c
Hamado Dene <hamado.dene@diennea.com>
2018-10-30 16:37:55 +0100

    fixing

--- ycsb-runner/full-ycsb_mysql.sh
+++ ycsb-runner/full-ycsb_mysql.sh
@@ -22,7 +22,7 @@ fi
 
 database="UsertableData"
 createdatabase="CREATE DATABASE $database;"
-dropdatabase="DROP DATABASE  $database;"
+dropdatabase="DROP DATABASE IF EXISTS  $database;"
 #droptable= "DROP TABLE usertable;"
 usedatabase="use $database;"
 createtable="CREATE TABLE usertable (YCSB_KEY VARCHAR(255) PRIMARY KEY, FIELD0 TEXT, FIELD1 TEXT,FIELD2 TEXT, FIELD3 TEXT, FIELD4 TEXT, FIELD5 TEXT, FIELD6 TEXT, FIELD7 TEXT,FIELD8 TEXT, FIELD9 TEXT);"
