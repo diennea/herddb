e304b0476265021da9ae9efa3ffca82d7c1b29de
Hamado Dene <hamado.dene@diennea.com>
2018-10-30 15:57:21 +0100

    new script for mysql benchmark

---
+++ ycsb-runner/full-ycsb_mysql.sh
@@ -0,0 +1,48 @@
+#/bin/bash
+
+HERE=$(dirname $0)
+HERE=$(realpath $HERE)
+YCSB_PATH=$1
+MYSQL_PROPERTIES=$2
+MYSQL_PATH=$3
+JDBC_PATH=$4
+WORKLOAD=$5
+JDBC_DRIVER=$(ls $JDBC_PATH/*connector*.jar)
+dbuser=$(grep "db.user" $MYSQL_PROPERTIES | cut -d "=" -f2)
+dbpasswd=$(grep "db.passwd" $MYSQL_PROPERTIES | cut -d "=" -f2)
+runservice="mysql-ssd.service"
+echo "Running YCSB workload $WORKLOAD from $YCSB_PATH"
+echo "Using JDBC Driver $JDBC_DRIVER"
+
+if [[ ! -d "$YCSB_PATH" ]]; then
+   echo "Directory $YCSB_PATH is not a valid directory"
+   exit 1
+fi
+
+
+database="UsertableData"
+createdatabase="CREATE DATABASE $database;"
+dropdatabase="DROP DATABASE  $database;"
+#droptable= "DROP TABLE usertable;"
+usedatabase="use $database;"
+createtable="CREATE TABLE usertable (YCSB_KEY VARCHAR(255) PRIMARY KEY, FIELD0 TEXT, FIELD1 TEXT,FIELD2 TEXT, FIELD3 TEXT, FIELD4 TEXT, FIELD5 TEXT, FIELD6 TEXT, FIELD7 TEXT,FIELD8 TEXT, FIELD9 TEXT);"
+
+$MYSQL_PATH/bin/mysql --socket $MYSQL_PATH/data/mysqld.sock -u $dbuser -p$dbpasswd << EOF
+$dropdatabase
+$createdatabase
+EOF
+
+$MYSQL_PATH/bin/mysql --socket $MYSQL_PATH/data/mysqld.sock -u $dbuser -p$dbpasswd  << EOF
+$usedatabase
+$createtable
+EOF
+
+
+
+
+
+
+$YCSB_PATH/bin/ycsb load jdbc -P $YCSB_PATH/workloads/$WORKLOAD -P $MYSQL_PROPERTIES  -cp $JDBC_PATH/mysql-connector*  -threads 200 -s
+echo "END_LOAD"
+$YCSB_PATH/bin/ycsb run jdbc -P $YCSB_PATH/workloads/$WORKLOAD -P $MYSQL_PROPERTIES -cp $JDBC_PATH/mysql-connector*  -threads 200 -s
+echo "END_FILE"
--- ycsb-runner/ycsb-report_mysql.sh
+++ ycsb-runner/ycsb-report_mysql.sh
@@ -1,25 +1,26 @@
 #/bin/bash
 
 
-VAR=$3
+VAR=$5
 HERE=$(dirname $0)
 HERE=$(realpath $HERE)
 YCSB_PATH=$1
-HERDDB_PATH=$(realpath $2)
+MYSQL_PATH=$(realpath $3)
+MYSQL_PROPERTIES=$2
 WORKLOAD=$@
-JDBC_DRIVER=$(ls $HERDDB_PATH/*jdbc*.jar)
-I=3
+I=5
 NARG=0 
 WORK=
-REPORT="Benchmarck.txt"
+REPORT="Benchmark.txt"
 L=0
 FILE_TEMP="file_temp/"
 FINAL_REPORT="REPORT_FINAL/"
-HERDDB="HERDDB_"
+JDBC_PATH=$4
+MYSQL="MYSQL_"
 
 
-if [[ -e $REPORT ]]; then 
-rm -rf $REPORT
+if [[ -e $MYSQL$REPORT ]]; then 
+rm -rf $MYSQL$REPORT
 fi 
 rm -rf $FILE_TEMP
 rm -rf $FINAL_REPORT
@@ -32,16 +33,16 @@ while [ $L -lt $VAR ]; do
  		WORK=${argv[$I]}
 		echo $WORK
 		rm -rf $REPORT
-		./full-ycsb.sh  $YCSB_PATH  $HERDDB_PATH  $WORK > $FILE_TEMP$WORK.txt
-		./parse_report.sh $WORK.txt $WORK$I.txt $WORK $VAR $FILE_TEMP $FINAL_REPORT $HERDDB_PATH
+		./full-ycsb_mysql.sh  $YCSB_PATH $MYSQL_PROPERTIES  $MYSQL_PATH $JDBC_PATH $WORK > $FILE_TEMP$WORK.txt
+		./parse_report.sh $WORK.txt $WORK$I.txt $WORK $VAR $FILE_TEMP $FINAL_REPORT $MYSQL_PATH
 		let I=I+1
 	done
-I=3
+I=5
 let L=L+1
 done
 
 
-cat  $FINAL_REPORT* > $HERDDB$REPORT
+cat  $FINAL_REPORT* > $MYSQL$REPORT
 
 
 rm -rf $FILE_TEMP
