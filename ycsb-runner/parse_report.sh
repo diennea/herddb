ed46a43646992f1bf4cb4c919ef4ee0c8655b6a8
Hamado Dene <hamado.dene@diennea.com>
2018-10-30 08:43:38 +0100

    Add load phase report

--- ycsb-runner/parse_report.sh
+++ ycsb-runner/parse_report.sh
@@ -14,7 +14,11 @@ REPORT="FULL_TOTAL_REPORT.txt"
 FILE_TEMP=$5
 FINAL_REPORT=$6
 HERD_PATH=$7
+LOAD_FILE="loadfile"
+LOAD=0
+MEDIA_LOAD=0
 grep -B50 END_FILE $FILE_TEMP$FILE_NAME > $FILE_TEMP$SAVE_FILE_NAME 
+grep -B50 "END_LOAD" $FILE_TEMP$FILE_NAME | grep " Throughput(ops/sec)" | cut -f 3 -d , |  awk '{$1=$1;print}' | awk -F"." '{print $1}' >> $FILE_TEMP$LOAD_FILE$SAVE_FILE_NAME
 
 if [[ ! -e $FILE_TEMP$NAME$FILE_NAME ]]; then
         grep "Throughput(ops/sec)" $FILE_TEMP$SAVE_FILE_NAME > $FILE_TEMP$TEMP2
@@ -27,18 +31,24 @@ else
         cut -f 3 -d, $FILE_TEMP$TEMP2  > $FILE_TEMP$TEMP3
         cat $FILE_TEMP$TEMP3 | awk '{$1=$1;print}' | awk -F"." '{print $1}' > $FILE_TEMP$NAME$FILE_NAME     #prende il risultato, ci toglie lo spazio iniziale e prende solamente la parte prima del punto
 fi
+
 rm -rf $FILE_TEMP$SAVE_FILE_NAME
 rm -rf $FILE_TEMP$TEMP
 rm -rf $FILE_TEMP$NAME.txt
 rm -rf $FILE_TEMP$TEMP2
 rm -rf $FILE_TEMP$TEMP3
-
 NUMERO_TENTATIVI=$4
 while read -r line   #scorro tutte le righe del file
 do
  let SUM=$SUM+$line  #calcolo della somma di tutte le righe presenti nel file
 done < $FILE_TEMP$NAME$FILE_NAME
-MEDIA=$(($SUM/$NUMERO_TENTATIVI)) #calcolo della media
 
-./final_report.sh $WORKLOAD $MEDIA $NUMERO_TENTATIVI $REPORT $FILE_TEMP $FINAL_REPORT $HERD_PATH
+while read -r line
+do 
+let LOAD=$LOAD+$line
+done < $FILE_TEMP$LOAD_FILE$SAVE_FILE_NAME 
+MEDIA=$(($SUM/$NUMERO_TENTATIVI)) #calcolo della media
+MEDIA_LOAD=$(($LOAD/$NUMERO_TENTATIVI))
+
+./final_report.sh $WORKLOAD $MEDIA $NUMERO_TENTATIVI $REPORT $FILE_TEMP $FINAL_REPORT $HERD_PATH $MEDIA_LOAD
 
