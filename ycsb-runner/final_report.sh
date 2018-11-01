8e1cc2c1f52f2ee6fab4ab9e059a7adb22e0feab
hamadodene <43852233+hamadodene@users.noreply.github.com>
2018-11-01 17:42:18 +0100

    YCBS reports for Mysql (#285)

--- ycsb-runner/final_report.sh
+++ ycsb-runner/final_report.sh
@@ -5,13 +5,16 @@ NAME="_final_report.txt"
 SYSTEM=$(uname -s -r)
 RAM=$(dmidecode -t 16 | grep "Maximum Capacity" | cut -d ':'  -f2)
 DATE=$(date)
-PROCESSOR=$(grep -m 1 'cpu cores' /proc/cpuinfo)
+
+PROCESSOR=$(grep -m 1 'cpu cores' /proc/cpuinfo| cut -d ":" -f2)
 THROUGHPUT=$2
 ATTEMPTS=$3
 REPORT=$4
 FILE_TEMP=$5
 FINAL_REPORT=$6
-HERD_PATH=$7
+DATABASE_PATH=$7
+MEDIA_LOAD=$8
+
 $JAVA_HOME/bin/java -version 2> $FILE_TEMPjavaversion.txt
 
 if [[ ! -e  $FILE_TEMP$WORKLOAD$NAME ]]; then
@@ -24,17 +27,16 @@ fi
 echo -e " " >> $FILE_TEMP$WORKLOAD$NAME
 echo -e "------------------------------------------------------------------- " >> $FILE_TEMP$WORKLOAD$NAME
 echo "$(grep "java version" $FILE_TEMPjavaversion.txt)" >> $FILE_TEMP$WORKLOAD$NAME
-echo -e "" >> $FILE_TEMP$WORKLOAD$NAME
-echo "Herddb=$HERD_PATH"  >> $FILE_TEMP$WORKLOAD$NAME
-echo "WORKLOAD=$WORKLOAD" >> $FILE_TEMP$WORKLOAD$NAME
-echo "SYSTEM=$SYSTEM" >> $FILE_TEMP$WORKLOAD$NAME
-echo "RAM=$RAM" >> $FILE_TEMP$WORKLOAD$NAME
-echo "$PROCESSOR" >> $FILE_TEMP$WORKLOAD$NAME
+
+echo "Database Folder=$DATABASE_PATH"  >> $FILE_TEMP$WORKLOAD$NAME
+echo "Workload=$WORKLOAD" >> $FILE_TEMP$WORKLOAD$NAME
+echo "System=$SYSTEM" >> $FILE_TEMP$WORKLOAD$NAME
+echo "Ram=$RAM" >> $FILE_TEMP$WORKLOAD$NAME
+echo "Number of processor=$PROCESSOR" >> $FILE_TEMP$WORKLOAD$NAME
 echo "Date=$DATE" >> $FILE_TEMP$WORKLOAD$NAME
 echo "Throughput=$THROUGHPUT" >> $FILE_TEMP$WORKLOAD$NAME
+echo "Load phase=$MEDIA_LOAD" >> $FILE_TEMP$WORKLOAD$NAME
 echo "Attempts=$ATTEMPTS"  >> $FILE_TEMP$WORKLOAD$NAME
-echo "Herddb=$HERD_PATH"  >> $FILE_TEMP$WORKLOAD$NAME
-
 
 mv $FILE_TEMP$WORKLOAD$NAME $FINAL_REPORT
  
