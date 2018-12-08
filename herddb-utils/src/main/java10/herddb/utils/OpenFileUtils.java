1a479761b8cccbc9ea1669843a75fd56f0f52b50
Enrico Olivelli <eolivelli@apache.org>
2018-12-08 09:49:28 +0100

    Better log message

--- herddb-utils/src/main/java10/herddb/utils/OpenFileUtils.java
+++ herddb-utils/src/main/java10/herddb/utils/OpenFileUtils.java
@@ -38,7 +38,7 @@ public class OpenFileUtils {
     private static final Logger LOG = Logger.getLogger(OpenFileUtils.class.getName());
 
     static {
-        LOG.info("Using O_DIRECT, available from JDK10+");
+        LOG.info("This JVM is able to use O_DIRECT");
     }
 
     public static FileChannel openFileChannelWithO_DIRECT(Path path, OpenOption... options) throws IOException {
