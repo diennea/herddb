ad3fbe2d6f787a863ea8ad6e5f1cb261bcdf0613
Amit Chavan <achavan1@gmail.com>
2019-06-04 08:56:38 -0700

    Adding license headers and more Unit test for show create table functionality

--- herddb-core/src/main/java/herddb/sql/functions/ShowCreateTableCalculator.java
+++ herddb-core/src/main/java/herddb/sql/functions/ShowCreateTableCalculator.java
@@ -1,3 +1,22 @@
+/*
+ Licensed to Diennea S.r.l. under one
+ or more contributor license agreements. See the NOTICE file
+ distributed with this work for additional information
+ regarding copyright ownership. Diennea S.r.l. licenses this file
+ to you under the Apache License, Version 2.0 (the
+ "License"); you may not use this file except in compliance
+ with the License.  You may obtain a copy of the License at
+
+ http://www.apache.org/licenses/LICENSE-2.0
+
+ Unless required by applicable law or agreed to in writing,
+ software distributed under the License is distributed on an
+ "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ KIND, either express or implied.  See the License for the
+ specific language governing permissions and limitations
+ under the License.
+
+ */
 package herddb.sql.functions;
 
 import herddb.core.AbstractTableManager;
@@ -29,7 +48,7 @@ public class ShowCreateTableCalculator {
         StringBuilder sb = new StringBuilder("CREATE TABLE "+tableSpace+"."+tableName);
         StringJoiner joiner = new StringJoiner(",","(", ")");
         for(Column c : t.getColumns()) {
-            joiner.add(c.name + " "+ ColumnTypes.typeToString(c.type));
+            joiner.add(c.name + " "+ ColumnTypes.typeToString(c.type)  + autoIncrementColumn(t,c));
         }
 
         if (t.getPrimaryKey().length > 0) {
@@ -41,12 +60,20 @@ public class ShowCreateTableCalculator {
 
             if (!indexes.isEmpty()) {
                 indexes.forEach( idx -> {
-                    joiner.add("Index "+idx.name + "("+Arrays.stream(idx.columnNames).collect(Collectors.joining(",")) + ")");
+                    joiner.add("INDEX "+idx.name + "("+Arrays.stream(idx.columnNames).collect(Collectors.joining(",")) + ")");
                 });
             }
         }
 
         sb.append(joiner.toString());
         return sb.toString();
+    }
+
+    private static String autoIncrementColumn(Table t, Column c) {
+        if (t.auto_increment && c.name.equals(t.primaryKey[0]) && (c.type == ColumnTypes.INTEGER || c.type == ColumnTypes.NOTNULL_INTEGER ||
+                c.type == ColumnTypes.LONG || c.type == ColumnTypes.NOTNULL_LONG )) {
+            return " auto_increment";
+        }
+        return "";
     }
 }
