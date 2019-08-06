b2459ce5b5467a268ee05e7fe14f170656adfe3c
Enrico Olivelli <eolivelli@gmail.com>
2019-08-06 12:04:16 +0200

    Ensure that pages are unloaded on table drop (#435)
    Fixes #434

--- herddb-core/src/test/java/herddb/core/DropTablespaceTest.java
+++ herddb-core/src/test/java/herddb/core/DropTablespaceTest.java
@@ -20,6 +20,10 @@
 package herddb.core;
 
 import static herddb.core.TestUtils.execute;
+import herddb.core.stats.TableManagerStats;
+import herddb.file.FileCommitLogManager;
+import herddb.file.FileDataStorageManager;
+import herddb.file.FileMetadataStorageManager;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
 
@@ -36,13 +40,21 @@ import herddb.model.StatementEvaluationContext;
 import herddb.model.TransactionContext;
 import herddb.model.commands.CreateTableSpaceStatement;
 import herddb.model.commands.DropTableSpaceStatement;
+import herddb.server.ServerConfiguration;
+import java.nio.file.Path;
+import org.apache.bookkeeper.stats.NullStatsLogger;
+import org.junit.Rule;
+import org.junit.rules.TemporaryFolder;
 
 /**
- * Drop Tablespace Tests
+ * Drop Tablespace and Tables Tests
  *
  * @author enrico.olivelli
  */
 public class DropTablespaceTest {
+
+    @Rule
+    public TemporaryFolder folder = new TemporaryFolder();
 
     @Test
     public void test() throws Exception {
@@ -87,4 +99,57 @@ public class DropTablespaceTest {
             }
         }
     }
+
+    @Test
+    public void testCleanPolicyOnDropTable() throws Exception {
+        String nodeId = "localhost";
+        ServerConfiguration serverConfig = new ServerConfiguration();
+        // very low memory
+        serverConfig.set(ServerConfiguration.PROPERTY_MAX_DATA_MEMORY, 2_000_000);
+        serverConfig.set(ServerConfiguration.PROPERTY_MAX_PK_MEMORY, 2_000_000);
+        Path dataPath = folder.newFolder("data").toPath();
+        Path logsPath = folder.newFolder("logs").toPath();
+        Path metadataPath = folder.newFolder("metadata").toPath();
+        Path tmoDir = folder.newFolder("tmoDir").toPath();
+
+        try (DBManager manager = new DBManager(nodeId,
+                new FileMetadataStorageManager(metadataPath),
+                new FileDataStorageManager(dataPath),
+                new MemoryCommitLogManager(false),
+                tmoDir, null, serverConfig, NullStatsLogger.INSTANCE)) {
+            manager.start();
+            CreateTableSpaceStatement st1 = new CreateTableSpaceStatement("tblspace1", Collections.singleton(nodeId), nodeId, 1, 0, 0);
+            manager.executeStatement(st1, StatementEvaluationContext.DEFAULT_EVALUATION_CONTEXT(), TransactionContext.NO_TRANSACTION);
+            manager.waitForTablespace("tblspace1", 10000);
+            execute(manager, "CREATE TABLE tblspace1.tsql (k1 string primary key,n1 int,s1 string)", Collections.emptyList());
+            execute(manager, "CREATE TABLE tblspace1.tsql2 (k1 string primary key,n1 int,s1 string)", Collections.emptyList());
+
+            for (int i = 0; i < 10000; i++) {
+                execute(manager, "INSERT INTO tblspace1.tsql (k1 ,n1 ,s1 ) values(?,?,?)",
+                        Arrays.asList(i, i, "jkhdskfhjdskfhskdjfhksdhfkshfkjhsdlkhflksjhfklshdkljhsdlkf"
+                        + "ksdjhfkdsflksdjhfklsdhjflksdhjflksdhjfklsdhjfklsdjhfsd"
+                        + "skjhsdlkfjhdslkfjhsdlkfjhsdlkfhjsklhdfkldshjflksjhdflkhsdf"
+                        + "jsdklfjhdslkfjhsdlkfhjslkjhfdlksjhdfklshjdflkhslkfdhslkfhslkfhslkdjfhklsdhjflkhsfd"
+                        + "jklsdhfksdjhflksdjhflksjhfdlkjhsdlfkhjslkfjhslkdfjhklsdhjfklhsdff"
+                        + "lksdjhflkshdjflkhjsdlkfjhsldkfjhslkfdfoo" + i));
+            }
+            // drop the table, any reference to the pages should be discarded
+            execute(manager, "DROP TABLE tblspace1.tsql", Collections.emptyList());
+
+            // write data to the second table, there will be no more space for table1 and if
+            // any page is still reference we should see the error of
+            // https://github.com/diennea/herddb/issues/434
+            for (int i = 0; i < 3000; i++) {
+                System.out.println("after "+i);
+                execute(manager, "INSERT INTO tblspace1.tsql2 (k1 ,n1 ,s1 ) values(?,?,?)",
+                        Arrays.asList(i, i, "jkhdskfhjdskfhskdjfhksdhfkshfkjhsdlkhflksjhfklshdkljhsdlkf"
+                        + "ksdjhfkdsflksdjhfklsdhjflksdhjflksdhjfklsdhjfklsdjhfsd"
+                        + "skjhsdlkfjhdslkfjhsdlkfjhsdlkfhjsklhdfkldshjflksjhdflkhsdf"
+                        + "jsdklfjhdslkfjhsdlkfhjslkjhfdlksjhdfklshjdflkhslkfdhslkfhslkfhslkdjfhklsdhjflkhsfd"
+                        + "jklsdhfksdjhflksdjhflksjhfdlkjhsdlfkhjslkfjhslkdfjhklsdhjfklhsdff"
+                        + "lksdjhflkshdjflkhjsdlkfjhsldkfjhslkfdfoo" + i));
+            }
+        }
+    }
+
 }
