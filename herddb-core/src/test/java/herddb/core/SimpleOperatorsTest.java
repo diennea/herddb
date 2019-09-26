9621fca98c26ca2b922d8b6c26645b6d126f6a93
Enrico Olivelli <enrico.olivelli@diennea.com>
2019-09-26 12:31:25 +0200

    Fix checkstyle

--- herddb-core/src/test/java/herddb/core/SimpleOperatorsTest.java
+++ herddb-core/src/test/java/herddb/core/SimpleOperatorsTest.java
@@ -32,8 +32,6 @@ import herddb.model.DataScanner;
 import herddb.model.StatementEvaluationContext;
 import herddb.model.TransactionContext;
 import herddb.model.commands.CreateTableSpaceStatement;
-import herddb.sql.CalcitePlanner;
-import herddb.sql.DDLSQLPlanner;
 import java.sql.Timestamp;
 import java.util.Arrays;
 import java.util.Collections;
