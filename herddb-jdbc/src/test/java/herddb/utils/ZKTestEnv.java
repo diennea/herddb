77ec246a5d384459aa0646b25203dccfe81bfbc7
Enrico Olivelli <enrico.olivelli@diennea.com>
2019-11-08 14:34:12 +0100

    Make JdbcDriverZookeeperTest less Flaky

--- herddb-jdbc/src/test/java/herddb/utils/ZKTestEnv.java
+++ herddb-jdbc/src/test/java/herddb/utils/ZKTestEnv.java
@@ -1,33 +1,37 @@
 /*
- Licensed to Diennea S.r.l. under one
- or more contributor license agreements. See the NOTICE file
- distributed with this work for additional information
- regarding copyright ownership. Diennea S.r.l. licenses this file
- to you under the Apache License, Version 2.0 (the
- "License"); you may not use this file except in compliance
- with the License.  You may obtain a copy of the License at
-
- http://www.apache.org/licenses/LICENSE-2.0
-
- Unless required by applicable law or agreed to in writing,
- software distributed under the License is distributed on an
- "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
- KIND, either express or implied.  See the License for the
- specific language governing permissions and limitations
- under the License.
-
+ * Licensed to Diennea S.r.l. under one
+ * or more contributor license agreements. See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership. Diennea S.r.l. licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ *
  */
-
 package herddb.utils;
 
 import java.nio.file.Path;
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.TimeUnit;
 import org.apache.bookkeeper.client.BookKeeperAdmin;
 import org.apache.bookkeeper.conf.ServerConfiguration;
 import org.apache.bookkeeper.proto.BookieServer;
 import org.apache.bookkeeper.zookeeper.ZooKeeperClient;
 import org.apache.curator.test.TestingServer;
 import org.apache.zookeeper.CreateMode;
+import org.apache.zookeeper.WatchedEvent;
+import org.apache.zookeeper.Watcher.Event.KeeperState;
 import org.apache.zookeeper.ZooDefs;
+import org.apache.zookeeper.ZooKeeper;
 
 public class ZKTestEnv implements AutoCloseable {
 
@@ -37,6 +41,15 @@ public class ZKTestEnv implements AutoCloseable {
 
     public ZKTestEnv(Path path) throws Exception {
         zkServer = new TestingServer(1282, path.toFile(), true);
+        CountDownLatch latch = new CountDownLatch(1);
+        ZooKeeper zk = new ZooKeeper(zkServer.getConnectString(), 100, (WatchedEvent event) -> {
+            System.out.println("ZK EVENT " + event);
+            if (event.getState() == KeeperState.SyncConnected) {
+                latch.countDown();
+            }
+        });
+        latch.await(1000, TimeUnit.SECONDS);
+        zk.close(1000);
         this.path = path;
     }
 
@@ -67,7 +80,6 @@ public class ZKTestEnv implements AutoCloseable {
 
         conf.setAllowLoopback(true);
         conf.setProperty("journalMaxGroupWaitMSec", 10); // default 200ms
-
 
         try (ZooKeeperClient zkc = ZooKeeperClient
                 .newBuilder()
