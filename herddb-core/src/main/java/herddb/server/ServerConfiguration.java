b4e8e710aca88f9948c0dcb037e722c21cc31955
Enrico Olivelli <enrico.olivelli@diennea.com>
2019-12-09 13:17:08 +0100

    fix checkstyle

--- herddb-core/src/main/java/herddb/server/ServerConfiguration.java
+++ herddb-core/src/main/java/herddb/server/ServerConfiguration.java
@@ -27,7 +27,6 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.nio.charset.StandardCharsets;
 import java.nio.file.Path;
-import java.util.Arrays;
 import java.util.List;
 import java.util.Properties;
 import java.util.logging.Level;
