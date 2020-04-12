1f70ca02af902684b9428f54f07d195407b731f8
Hamado Dene - Diennea <hamado.dene@diennea.com>
2020-04-13 00:10:23 +0200

    Fix checkstyle

--- herddb-utils/src/main/java/herddb/proto/Pdu.java
+++ herddb-utils/src/main/java/herddb/proto/Pdu.java
@@ -58,7 +58,7 @@ public class Pdu implements AutoCloseable {
     public static final byte TYPE_SASL_TOKEN_SERVER_RESPONSE = 101;
     public static final byte TYPE_SASL_TOKEN_MESSAGE_TOKEN = 102;
     public static final byte TYPE_PREPARE_STATEMENT = 103;
-    public static final byte TYPE_PREPARE_STATEMENT_RESULT = 104;    
+    public static final byte TYPE_PREPARE_STATEMENT_RESULT = 104;
 
     public static final byte FLAGS_ISREQUEST = 1;
     public static final byte FLAGS_ISRESPONSE = 2;
