026f6d94cc1055ca4095d967a549377718bb6e97
Enrico Olivelli <eolivelli@apache.org>
2020-06-28 16:44:42 +0200

    fix pom

--- herddb-thirdparty/openjpa/src/main/java/herddb/openjpa/DBDictionary.java
+++ herddb-thirdparty/openjpa/src/main/java/herddb/openjpa/DBDictionary.java
@@ -16,7 +16,6 @@
 package herddb.openjpa;
 
 import java.sql.SQLException;
-import java.util.Arrays;
 import javax.sql.DataSource;
 import org.apache.commons.dbcp.BasicDataSource;
 import org.apache.openjpa.lib.jdbc.DelegatingDataSource;
@@ -28,37 +27,33 @@ public class DBDictionary extends org.apache.openjpa.jdbc.sql.DBDictionary {
 
     public DBDictionary() {
         platform = "HerdDB";
-        validationSQL = "SELECT CURRENT_TIMESTAMP FROM herd";
 
-        datePrecision = MILLI;
-
-        autoAssignClause = "AUTO_INCREMENT";
-
-        reservedWordSet.addAll(Arrays.asList(new String[]{
-            "AUTO_INCREMENT", "USER", "LANGUAGE", "PERIOD", "VALUE"
-        }));
-
-        invalidColumnWordSet.addAll(Arrays.asList(new String[]{
-            "ADD", "ALL", "ALTER", "AND", "AS", "ASC", "BETWEEN", "BINARY",
-            "BLOB", "BOTH", "BY", "CASCADE", "CASE", "CHANGE", "CHAR",
-            "CHARACTER", "CHECK", "COLLATE", "COLUMN", "CONSTRAINT", "CONTINUE",
-            "CONVERT", "CREATE", "CROSS", "CURRENT_DATE", "CURRENT_TIME",
-            "CURRENT_TIMESTAMP", "CURRENT_USER", "CURSOR", "DEC", "DECIMAL",
-            "DECLARE", "DEFAULT", "DELETE", "DESC", "DESCRIBE", "DISTINCT",
-            "DOUBLE", "DROP", "ELSE", "END-EXEC", "EXISTS", "FALSE", "FETCH",
-            "FLOAT", "FLOAT4", "FOR", "FOREIGN", "FROM", "GRANT", "GROUP",
-            "HAVING", "IN", "INFILE", "INNER", "INSENSITIVE", "INSERT", "INT",
-            "INT1", "INT2", "INT4", "INTEGER", "INTERVAL", "INTO", "IS", "JOIN",
-            "KEY", "LEADING", "LEFT", "LIKE", "LOAD", "MATCH", "MEDIUMINT",
-            "NATURAL", "NOT", "NULL", "NUMERIC", "ON", "OPTION", "OR", "ORDER",
-            "OUTER", "OUTFILE", "PRECISION", "PRIMARY", "PROCEDURE", "READ",
-            "REAL", "REFERENCES", "REPLACE", "RESTRICT", "REVOKE", "RIGHT",
-            "SCHEMA", "SELECT", "SET", "SMALLINT", "SQL", "SQLSTATE",
-            "STARTING", "TABLE", "THEN", "TO", "TRAILING", "TRUE", "UNION",
-            "UNIQUE", "UNSIGNED", "UPDATE", "USAGE", "USING", "VALUES",
-            "VARCHAR", "VARYING", "WHEN", "WHERE", "WITH", "WRITE", "ZEROFILL",
-            "INDEX", "AUTO_INCREMENT", "USER", "LANGUAGE", "PERIOD", "VALUE", "VALUES"
-        }));
+//         It looks like this stuff does not work, it still does create tables with 'user' name
+//        reservedWordSet.addAll(Arrays.asList(new String[]{
+//            "AUTO_INCREMENT", "USER", "LANGUAGE", "PERIOD", "VALUE"
+//        }));
+//
+//        invalidColumnWordSet.addAll(Arrays.asList(new String[]{
+//            "ADD", "ALL", "ALTER", "AND", "AS", "ASC", "BETWEEN", "BINARY",
+//            "BLOB", "BOTH", "BY", "CASCADE", "CASE", "CHANGE", "CHAR",
+//            "CHARACTER", "CHECK", "COLLATE", "COLUMN", "CONSTRAINT", "CONTINUE",
+//            "CONVERT", "CREATE", "CROSS", "CURRENT_DATE", "CURRENT_TIME",
+//            "CURRENT_TIMESTAMP", "CURRENT_USER", "CURSOR", "DEC", "DECIMAL",
+//            "DECLARE", "DEFAULT", "DELETE", "DESC", "DESCRIBE", "DISTINCT",
+//            "DOUBLE", "DROP", "ELSE", "END-EXEC", "EXISTS", "FALSE", "FETCH",
+//            "FLOAT", "FLOAT4", "FOR", "FOREIGN", "FROM", "GRANT", "GROUP",
+//            "HAVING", "IN", "INFILE", "INNER", "INSENSITIVE", "INSERT", "INT",
+//            "INT1", "INT2", "INT4", "INTEGER", "INTERVAL", "INTO", "IS", "JOIN",
+//            "KEY", "LEADING", "LEFT", "LIKE", "LOAD", "MATCH", "MEDIUMINT",
+//            "NATURAL", "NOT", "NULL", "NUMERIC", "ON", "OPTION", "OR", "ORDER",
+//            "OUTER", "OUTFILE", "PRECISION", "PRIMARY", "PROCEDURE", "READ",
+//            "REAL", "REFERENCES", "REPLACE", "RESTRICT", "REVOKE", "RIGHT",
+//            "SCHEMA", "SELECT", "SET", "SMALLINT", "SQL", "SQLSTATE",
+//            "STARTING", "TABLE", "THEN", "TO", "TRAILING", "TRUE", "UNION",
+//            "UNIQUE", "UNSIGNED", "UPDATE", "USAGE", "USING", "VALUES",
+//            "VARCHAR", "VARYING", "WHEN", "WHERE", "WITH", "WRITE", "ZEROFILL",
+//            "INDEX", "AUTO_INCREMENT", "USER", "LANGUAGE", "PERIOD", "VALUE", "VALUES"
+//        }));
 
         forUpdateClause = "";
 
@@ -87,5 +82,6 @@ public class DBDictionary extends org.apache.openjpa.jdbc.sql.DBDictionary {
             }
         }
     }
+
 
 }
