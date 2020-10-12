7c7b110cb9f007a92b7f06efa448c3d1c84d570a
Enrico Olivelli <eolivelli@apache.org>
2020-10-12 12:58:37 +0200

    remove debug

--- herddb-utils/src/main/java10/herddb/utils/OpenFileUtils.java
+++ herddb-utils/src/main/java10/herddb/utils/OpenFileUtils.java
@@ -40,7 +40,7 @@ public class OpenFileUtils {
     private static final Logger LOG = Logger.getLogger(OpenFileUtils.class.getName());
 
     static {
-        LOG.info("This JVM is able to use O_DIRECT");
+        LOG.fine("This JVM is able to use O_DIRECT");
     }
 
     public static FileChannel openFileChannelWithO_DIRECT(Path path, OpenOption... options) throws IOException {
