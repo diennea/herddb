ebf634889cdddce9558eca30e5ad17210d1ec74a
Enrico Olivelli <enrico.olivelli@diennea.com>
2020-11-02 16:01:03 +0100

    Disable useSchemaName and use 'native' SchemaFactory

--- herddb-thirdparty/openjpa/src/main/java/herddb/openjpa/DBDictionary.java
+++ herddb-thirdparty/openjpa/src/main/java/herddb/openjpa/DBDictionary.java
@@ -32,6 +32,7 @@ public class DBDictionary extends org.apache.openjpa.jdbc.sql.DBDictionary {
         supportsCascadeUpdateAction = false;
         schemaCase = SCHEMA_CASE_LOWER;
         delimitedCase = SCHEMA_CASE_PRESERVE;
+//        useSchemaName = false;
 
         // make OpenJPA escape everything, because Apache Calcite has a lot of reserved words, like 'User', 'Value'...
         setDelimitIdentifiers(true);
