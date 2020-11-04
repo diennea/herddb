9db739f490bc6e6afe3094a19e3a748b7b95cc5e
Enrico Olivelli <enrico.olivelli@diennea.com>
2020-11-04 13:25:38 +0100

    improve tests

--- herddb-core/src/test/java/herddb/cluster/WaitForBookiesTest.java
+++ herddb-core/src/test/java/herddb/cluster/WaitForBookiesTest.java
@@ -86,7 +86,7 @@ public class WaitForBookiesTest {
             server_1.start();
             server_1.getManager().setActivatorPauseStatus(true);
             Exception err = TestUtils.expectThrows(Exception.class, () -> {
-                server_1.waitForTableSpaceBoot(TableSpace.DEFAULT, 20000, true);
+                server_1.waitForTableSpaceBoot(TableSpace.DEFAULT, 5000, true);
             });
             assertEquals("TableSpace " + TableSpace.DEFAULT + " not started within 5000 ms", err.getMessage());
         }
