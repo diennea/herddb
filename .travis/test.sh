ab1e6a2fdfda7c6d1dc642c46f9d80d122e9d25f
Enrico Olivelli <enrico.olivelli@diennea.com>
2020-11-04 08:15:02 +0100

    forkCount=4

--- .travis/test.sh
+++ .travis/test.sh
@@ -4,4 +4,4 @@ mvn -v
 # build and validate
 mvn checkstyle:check install spotbugs:check apache-rat:check -Pjenkins -DskipTests
 # run tests
-mvn verify -Pjenkins -Dmaven.test.redirectTestOutputToFile=true -DforkCount=2 -Dherddb.file.requirefsync=false jacoco:report coveralls:report
+mvn verify -Pjenkins -Dmaven.test.redirectTestOutputToFile=true -DforkCount=4 -Dherddb.file.requirefsync=false jacoco:report coveralls:report
