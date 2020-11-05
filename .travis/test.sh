90c58ff7da65eaa562cd3c39f9d8f189f2378c97
Enrico Olivelli <enrico.olivelli@diennea.com>
2020-11-05 17:04:20 +0100

    xx

--- .travis/test.sh
+++ .travis/test.sh
@@ -5,4 +5,4 @@ mvn -v
 mvn checkstyle:check install spotbugs:check apache-rat:check -Pjenkins -DskipTests
 # run tests
 #mvn verify -Pjenkins -Dmaven.test.redirectTestOutputToFile=false -DforkCount=1 -Dherddb.file.requirefsync=false jacoco:report coveralls:report -Dtest=UseVirtualTableSpaceIdWithZookKeeperTest
-mvn verify -Pjenkins -Dmaven.test.redirectTestOutputToFile=false -DforkCount=1 -Dherddb.file.requirefsync=false jacoco:report -Dtest=UseVirtualTableSpaceIdWithZookKeeperTest
+mvn verify -Pjenkins -Dmaven.test.redirectTestOutputToFile=false -DforkCount=1 -Dherddb.file.requirefsync=false jacoco:report -Dtest=UseVirtualTableSpaceIdWithZookKeeperTest -DfailIfNoTests=false
