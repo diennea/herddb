d85a8901e7e22bddb766f96efcfb0e6a666288d4
Enrico Olivelli <enrico.olivelli@diennea.com>
2020-11-06 08:32:25 +0100

    split job into stages

--- .travis/test.sh
+++
@@ -1,8 +0,0 @@
-#bin/bash
-set -x -e
-mvn -v
-# build and validate
-mvn checkstyle:check install spotbugs:check apache-rat:check -Pjenkins -DskipTests
-# run tests
-#mvn verify -Pjenkins -Dmaven.test.redirectTestOutputToFile=false -DforkCount=1 -Dherddb.file.requirefsync=false jacoco:report coveralls:report -Dtest=UseVirtualTableSpaceIdWithZookKeeperTest
-mvn verify -Pjenkins -Dmaven.test.redirectTestOutputToFile=false -DforkCount=1 -Dherddb.file.requirefsync=false jacoco:report -Dtest=UseVirtualTableSpaceIdWithZookKeeperTest -DfailIfNoTests=false
