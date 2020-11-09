50711748c8110116d418f079976ff3d095fa73d3
Enrico Olivelli <enrico.olivelli@diennea.com>
2020-11-09 12:09:37 +0100

    remove duplicate work

--- .travis/test-other.sh
+++ .travis/test-other.sh
@@ -1,8 +1,6 @@
 #bin/bash
 set -x -e
 mvn -v
-# build and validate
-mvn checkstyle:check install spotbugs:check apache-rat:check -Pjenkins -DskipTests
 # run tests up to herddb-net
 mvn verify -Pjenkins -Dmaven.test.redirectTestOutputToFile=true -DforkCount=1 -Dherddb.file.requirefsync=false -am -pl herddb-net
 # skip herddb-core
