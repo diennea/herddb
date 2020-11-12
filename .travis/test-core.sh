a7a45185ec55be014e87bcd754a0aae3bf8062f2
Enrico Olivelli <enrico.olivelli@diennea.com>
2020-11-12 12:50:01 +0100

    remove travis, update JaCoCo

--- .travis/test-core.sh
+++
@@ -1,5 +0,0 @@
-#bin/bash
-set -x -e
-mvn -v
-# run tests in herddb-core
-mvn install -Pjenkins -Dmaven.test.redirectTestOutputToFile=true -DforkCount=2 -Dherddb.file.requirefsync=false -f herddb-core/pom.xml
--- .travis/test-other.sh
+++
@@ -1,8 +0,0 @@
-#bin/bash
-set -x -e
-mvn -v
-# run tests up to herddb-net
-mvn verify -Pjenkins -Dmaven.test.redirectTestOutputToFile=true -DforkCount=1 -Dherddb.file.requirefsync=false -am -pl herddb-net
-# skip herddb-core
-# resume from herddb-jdbc
-mvn verify -Pjenkins -Dmaven.test.redirectTestOutputToFile=true -DforkCount=1 -Dherddb.file.requirefsync=false -rf herddb-jdbc
